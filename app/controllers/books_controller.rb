595a599c9a006823094f45fd5f3078d56e9a8856
yusukenoiri <yusuke.noiri@gmail.com>
2021-04-04 06:53:13 +0000

    [Update]edit,destroy,update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,57 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+    # _newとかにして部分テンプレートにしてもいい
+    # _new 部分テンプレート、 renderで呼び出すこともできる
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was succesfully created."
+    redirect_to book_path(@book.id)
+    # redirectはコントローラーを通す
+    else
+      @books = Book.all
+      # コントローラーを通さないので、indexの中身を定義するため
+      render 'index'
+      # renderはコントローラーを通さない,renderされた後にindex.htmlにいく、if error文に引っかかり表示される
+   end
   end
 
   def edit
+    @book = Book.find(params[:id])
+    # newの場合インスタンスが空、editの場合すでにデータが入っている。それを判断するためにcontrollerを書き、
+    # createアクションかupdateアクションに処理を引き継いでくれる
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was succesfully updated."
+    redirect_to book_path(@book.id)
+    else
+    render 'edit'
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
+    # params.require(:book).permit(:title, :body)
     params.require(:book).permit(:title, :body)
     # params.require(:モデル名).permit(:カラム名1, :カラム名2, ...)
     # Strong Parametersを使うと、フォームからのデータを受け取れるようになる
   end
-  
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+
+validates :title, presence: true
+validates :body, presence: true
+# bookモデルが持っているカラム名をかく、Bookのテーブル
+  
 end
